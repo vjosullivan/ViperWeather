2fe42ad168838854cb8503563741b3ca496e8042
vjosullivan <vjosullivan@hotmail.com>
2018-04-15 22:17:55 +0100

    Initial Commit

---
+++ ViperWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViperWeather
+//
+//  Created by Vincent O'Sullivan on 15/04/2018.
+//  Copyright © 2018 Vincent O'Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
